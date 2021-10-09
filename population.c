84f53d1e74843e326ffd22a9a3d9d661182438ec
ak-magnificent <ak-magnificent@users.noreply.github.com>
2021-10-09 09:12:03 +0000

    automated commit by check50 [check50=True]

--- population.c
+++ population.c
@@ -1,31 +1,33 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    // First prompt to enter the population size
+    // i is for initial population size
     int i ;
     do
         {
+            //Propmt to initial population size.
             i = get_int("What's the starting population size?\n");
         }
     while (i < 9);
+    // f is for final population size
     int f ;
     do
         {
-            // Prompt to take end population size
+            // Prompt to ask end population size
             f = get_int("What's the final population size?\n");
         }
     while (f < i);
     // y = years required
     int y = 0 ;
-    // this loop checks for difference to be zero and increments the years on each round
-    int d = f - i ;
-    while (d > 0)
+    //This loop is to calculate the no of years.
+    while (i < f)
         {
-            d = (d - i/3 + i/4) ;
+            i = i + ( i/3 - i/4 ) ;
             y++;
         }
-        // TODO: Print number of years
+    //Print number of years.
     printf("Years: %i", y);
 }
