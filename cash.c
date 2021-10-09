58fd609acf23eafa7f55971eed03f962945079da
ak-magnificent <ak-magnificent@users.noreply.github.com>
2021-10-09 12:03:26 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -6,31 +6,36 @@ int main(void)
 {
     float cash ;
     do
-        {
-            cash = get_float("Change owed: ");
-        }
-    while (cash<0);
+    {
+        // Get user input in x.xx format
+        cash = get_float("Change owed: ");
+    }
+    while (cash < 0);
     int cents = round(cash * 100);
-    int i=0;
-    while (cents>=25)
-        {
-            cents = (cents-25);
-            i++;
-        }
-    while (cents>=10)
-        {
-            cents = (cents-10);
-            i++;
-        }
-    while (cents>=5)
-        {
-            cents = (cents-5);
-            i++;
-        }
-    while (cents>0)
-        {
-            cents = (cents-1);
-            i++;
-        }
+    int i = 0;
+    //checks for 25 c
+    while (cents >= 25)
+    {
+        cents = (cents - 25);
+        i++;
+    }
+    // checks for 10 c
+    while (cents >= 10)
+    {
+        cents = (cents - 10);
+        i++;
+    }
+    // checks for 5 c
+    while (cents >= 5)
+    {
+        cents = (cents - 5);
+        i++;
+    }
+    // checks for 1 c
+    while (cents >= 1)
+    {
+        cents = (cents - 1);
+        i++;
+    }
     printf("%i", i);
 }
