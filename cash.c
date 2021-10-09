07b8e61ba4cb65b18a0c8d28f5756fa96b83e51f
ak-magnificent <ak-magnificent@users.noreply.github.com>
2021-10-09 11:55:34 +0000

    automated commit by check50 [check50=True]

---
+++ cash.c
@@ -0,0 +1,36 @@
+#include<math.h>
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void)
+{
+    float cash ;
+    do
+        {
+            cash = get_float("Change owed: ");
+        }
+    while (cash<0);
+    int cents = round(cash * 100);
+    int i=0;
+    while (cents>25)
+        {
+            cents = (cents-25);
+            i++;
+        }
+    while (cents>10)
+        {
+            cents = (cents-10);
+            i++;
+        }
+    while (cents>5)
+        {
+            cents = (cents-5);
+            i++;
+        }
+    while (cents>0)
+        {
+            cents = (cents-1);
+            i++;
+        }
+    printf("%i", i);
+}
