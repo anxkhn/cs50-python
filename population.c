fa18c674417ae749d7dd36fe424802868fdf888f
ak-magnificent <ak-magnificent@users.noreply.github.com>
2021-10-09 09:17:52 +0000

    automated commit by submit50

--- population.c
+++ population.c
@@ -7,27 +7,28 @@ int main(void)
     // i is for initial population size
     int i ;
     do
-        {
-            //Propmt to initial population size.
-            i = get_int("What's the starting population size?\n");
-        }
+    {
+        //Propmt to initial population size.
+        i = get_int("What's the starting population size?\n");
+    }
     while (i < 9);
     // f is for final population size
     int f ;
     do
-        {
-            // Prompt to ask end population size
-            f = get_int("What's the final population size?\n");
-        }
+    {
+        // Prompt to ask end population size
+        f = get_int("What's the final population size?\n");
+    }
     while (f < i);
     // y = years required
     int y = 0 ;
     //This loop is to calculate the no of years.
     while (i < f)
-        {
-            i = i + ( i/3 - i/4 ) ;
-            y++;
-        }
+    {
+        
+        i = i + (i / 3 - i / 4) ;
+        y++;
+    }
     //Print number of years.
     printf("Years: %i", y);
 }
