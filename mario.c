57d7e402ef0b2f9bf647a88ad2dc3bad11d3b5b5
ak-magnificent <ak-magnificent@users.noreply.github.com>
2021-10-09 11:05:51 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -5,21 +5,26 @@ int main(void)
 {
     // User input
     int n ;
+    // Loop to get output within range.
     do
     {
         n = get_int("Enter Number :\n");
     }
-    while ( 0 >= n || n >= 9 ) ;
+    while (0 >= n || n >= 9) ;
+    // Loop to print output
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < (n-i-1); j++)
+        for (int j = 0; j < (n - i - 1); j++)
         {
+            //Prints blank Space
             printf(" ");
         }
         for (int j = 0; j <= i; j++)
         {
+            // Prints required HASH
             printf("#");
         }
-    printf("\n");
+        //Prints new line
+        printf("\n");
     }
 }
