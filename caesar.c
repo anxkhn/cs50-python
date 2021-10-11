c9ac2be7e6a4fc7bbf56960c3c71cdd0dbf96333
ak-magnificent <ak-magnificent@users.noreply.github.com>
2021-10-11 11:19:03 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -1,22 +1,31 @@
-#include <cs50.h>
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <cs50.h>
+
 
 int main(int argc, string argv[])
 {
     int key = 0;
-    if (argc != 2 || atoi(argv[1]) < 0)
+    for (int i = 0; i < strlen(argv[1]); i ++)
+    {
+        if (isalpha(argv[1][i]))
+        {
+            printf ("Enter a valid input.\n");
+            return 1;
+        }
+    }
+    if (argc != 2)
     {
         // warn user and re enter
-        printf("Enter a single command-line arg: non-negative integer.\n");
+        printf("Enter a single command-line. \n");
         return 1;
     }
     else
     {
         // convert key to integer then store the key
-        key = atoi(argv[1]);
+        key = atoi(argv[1])%26;
     }
     string word = get_string("plaintext:  ");
     int len = strlen(word);
@@ -26,7 +35,7 @@ int main(int argc, string argv[])
         int x = word[i] ;
         if (x <= 'Z' && x >= 'A')
         {
-            x = x + (key%26);
+            x = x + key;
             if (x > 'Z')
             {
                 x = x - 26 ;
@@ -34,7 +43,7 @@ int main(int argc, string argv[])
         }
         if (x <= 'z' && x >= 'a')
         {
-            x = x + (key%26);
+            x = x + key;
             if (x > 122 )
             {
                 x = x - 26 ;
