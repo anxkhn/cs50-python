7d794ae8ff09434924c8172354cfdfee675ec0b4
ak-magnificent <ak-magnificent@users.noreply.github.com>
2021-10-11 09:13:22 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -30,14 +30,14 @@ int main(void)
     int index = round(0.0588 * L - 0.296 * S - 15.8);
     if (index < 1 )
     {
-        printf("Before Grade 1");
+        printf("Before Grade 1\n");
     }
     else if (index >= 16)
     {
-        printf("Grade 16+");
+        printf("Grade 16+\n");
     }
     else
     {
-        printf("Grade %i", index);
+        printf("Grade %i\n", index);
     }
 }
