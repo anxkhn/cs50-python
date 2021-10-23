765909012bf1437e55f87b7be9435d725c5e9c9a
ak-magnificent <ak-magnificent@users.noreply.github.com>
2021-10-23 14:49:37 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -3,26 +3,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Convert image to grayscale
+//    Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Iterate through each pixel
+    //    go thru each pixel
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            // Extract values of colors
+            //    get values of colors
             int r = image[i][j].rgbtRed;
             int g = image[i][j].rgbtGreen;
             int b = image[i][j].rgbtBlue;
 
-            // Find average
-//            float average = (float)(r + g + b) / 3;
-//            int avg = (int) round(average);
-            int avg =  ceil((r + g + b) / 3);
+            //    Find average
+            float average = (float)(r + g + b) / 3;
+            int avg = (int) round(average);
 
-
-            // Replace original rgb values with average
+            //    Replace values
             image[i][j].rgbtRed = avg;
             image[i][j].rgbtGreen = avg;
             image[i][j].rgbtBlue = avg;
@@ -31,24 +29,24 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
-// Convert image to sepia
+//    Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Iterate through each pixel
+    //    go thru each pixel
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            // Extract values of colors
+            //    get values of colors
             int r = image[i][j].rgbtRed;
             int g = image[i][j].rgbtGreen;
             int b = image[i][j].rgbtBlue;
 
-            // make sepia
+            //    make sepia
             int sr = round(.393 * r + .769 * g + .189 * b);
             int sg = round(.349 * r + .686 * g + .168 * b);
             int sb = round(.272 * r + .534 * g + .131 * b);
-            // Checks to make sure each rgb value is valid (less then 255) and replaces it. Else sets to 255
+            //    Checks (255) error
             if (sr < 255)
             {
                 image[i][j].rgbtRed = sr;
@@ -80,14 +78,74 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     }
     return;
 }
-// Reflect image horizontally
+//    Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    RGBTRIPLE *holder = calloc(3, 1);
+    if (holder == NULL)
+    {
+        return;
+    }
+
+    //    flip code
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0, n = width / 2; j < n; j++)
+        {
+            *holder = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = *holder;
+        }
+    }
+    free(holder);
     return;
 }
 
-// Blur image
+//    Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp_image[height][width];
+    //    two loops
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int fRed = 0;
+            int fGreen = 0;
+            int fBlue = 0;
+            int base = 0;
+
+            //    get avg
+            for (int r = -1; r <= 1; r++)
+            {
+                for (int c = -1; c <= 1; c++)
+                {
+                    //    corner case
+                    if (i + r >= 0 && i + r <= height - 1 && j + c >= 0 && j + c <= width - 1)
+                    {
+                        fRed = fRed + image[i + r][j + c].rgbtRed;
+                        fGreen = fGreen + image[i + r][j + c].rgbtGreen;
+                        fBlue = fBlue + image[i + r][j + c].rgbtBlue;
+                        base++;
+                    }
+                }
+            }
+            //    final
+            temp_image[i][j].rgbtRed = (int) round((float) fRed / (float) base);
+            temp_image[i][j].rgbtGreen = (int) round((float) fGreen / (float) base);
+            temp_image[i][j].rgbtBlue = (int) round((float) fBlue / (float) base);
+
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp_image[i][j];
+        }
+    }
+
     return;
 }
