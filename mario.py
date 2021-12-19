435efab5c53f271beb36091aa3747637ba7280c6
ak-magnificent <ak-magnificent@users.noreply.github.com>
2021-12-19 07:00:41 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -2,10 +2,11 @@ from cs50 import get_int
 
 while True:
     height = get_int("Height: ")
+# from cs50 2o21 fall
     if height > 0 and height < 9:
         for i in range(height):
             print(" " * (height - i - 1), end="")
             print("#" * (i + 1), end="")
             print()
         break
-#different method than C , looked from cs50 lecture
+# different method than C
