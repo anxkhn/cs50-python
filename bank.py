de7c3b4f9220459d8d50d8e85f5672b6e3265d27
anxkhn <anxkhn@users.noreply.github.com>
2022-07-20 18:01:45 +0000

    automated commit by check50 [check50=True]

---
+++ bank.py
@@ -0,0 +1,10 @@
+name = str(input().strip().lower())
+if name[0] == "h":
+    if name[1] == "e":
+        print("$0")
+    else:
+        print("$20")
+else:
+    print("$100")
+
+# & name[2] == "l" & name[3] == "l" & name[4] == "o"
