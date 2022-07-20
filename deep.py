bd354b36ff0fa82b644fb55de0c926eb1e2f5b41
anxkhn <anxkhn@users.noreply.github.com>
2022-07-20 17:46:35 +0000

    automated commit by check50 [check50=True]

--- deep.py
+++ deep.py
@@ -1,5 +1,5 @@
 name = str(input("What the answer to the Great Question of Life, the Universe and Everything? ").strip().lower())
-if name == "42" or name == "forty two":
+if name == "42" or name == "forty two" or name == "forty-two":
     print("Yes")
 else:
     print("No")
