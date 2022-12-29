a2bf5fde3746a1dccf85c957eb93e186e90ccc56
anxkhn <anxkhn@users.noreply.github.com>
2022-12-29 16:51:02 +0530

    automated commit by check50 [check50=True]

--- fuel.py
+++ fuel.py
@@ -8,13 +8,13 @@ def get_val(prompt):
         if "/" in x:
             x = x.split("/")
             try:
-                frac = int(x[0])/int(x[1])
+                frac = int(x[0])/int(x[1])*100
                 #print(frac)
             except:
                 pass
-            if frac == 1:
+            if frac >= 99:
                 return "F"
-            elif frac == 0:
+            elif frac <= 1:
                 return "E"
             else:
                 return (str(frac*100)+"%")
