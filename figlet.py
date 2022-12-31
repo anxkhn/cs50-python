f96883a83a4b67b8a3d36ed9bc52a2c00ed01675
anxkhn <anxkhn@users.noreply.github.com>
2022-12-31 16:05:40 +0530

    automated commit by check50 [check50=True]

--- figlet.py
+++ figlet.py
@@ -5,11 +5,12 @@ figlet = Figlet()
 
 
 
-if sys.argv[1] == "--font" or sys.argv[1] == "-f":
-    s = input("Enter Text : ")
-    f = sys.argv[2]
-    figlet.setFont(font=f)
-    print(figlet.renderText(s))
+if len(sys.argv)== 3:
+    if sys.argv[1] == "--font" or sys.argv[1] == "-f":
+        s = input("Enter Text : ")
+        f = sys.argv[2]
+        figlet.setFont(font=f)
+        print(figlet.renderText(s))
 elif len(sys.argv) == 1:
     s = input("Enter Text : ")
     f = random.choice(figlet.getFonts())
