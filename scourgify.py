f1af0daa066c157ae900cec297a0b3e7b409ebe8
anxkhn <anxkhn@users.noreply.github.com>
2023-04-27 23:03:20 +0530

    automated commit by check50 [check50=True]

--- scourgify.py
+++ scourgify.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def main():
     check_command_line_arg()
     output = []
@@ -7,16 +8,17 @@ def main():
     sys.argv[1]
     try:
         file1 = open(sys.argv[1])
-        read_content = file1.read().replace('"', "").replace(" ", "")
+        read_content = file1.readlines()
         file1.close()
-        print(read_content)
 
     except FileNotFoundError:
         sys.exit(f"Could not read {sys.argv[1]}")
 
     # Write new csv file
     with open(sys.argv[2], "w") as file:
-        file.write(read_content)
+        for i in read_content:
+            file.write(i)
+            file.write("\n")
 
 
 def check_command_line_arg():
@@ -28,6 +30,7 @@ def check_command_line_arg():
     # Check if it is a CSV file
     if ".csv" not in sys.argv[1] or ".csv" not in sys.argv[2]:
         sys.exit("Not a CSV file")
+
 
 if __name__ == "__main__":
     main()
