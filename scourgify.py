a092fc146e4f20926ca1ccc19589b86395f0cab6
anxkhn <anxkhn@users.noreply.github.com>
2023-06-16 14:42:13 +0530

    automated commit by check50 [check50=True]

--- scourgify.py
+++ scourgify.py
@@ -1,34 +1,30 @@
+import csv
 import sys
 
-
 def main():
-    check_command_line_arg()
-    output = []
-    # Try to open the file
-    sys.argv[1]
+    command_line_check()
+    op = []
     try:
-        file1 = open(sys.argv[1])
-        read_content = file1.readlines()
-        file1.close()
+        with open(sys.argv[1]) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                splited = row['name'].split(",")
+                op.append({"first": splited[1].lstrip(), "last": splited[0], "house": row['house']})
 
     except FileNotFoundError:
         sys.exit(f"Could not read {sys.argv[1]}")
 
-    # Write new csv file
     with open(sys.argv[2], "w") as file:
-        for i in read_content:
-            x = i.replace(" ","").replace('"', "")
-            file.write(x)
-            file.write("\n")
+        writer = csv.DictWriter(file, fieldnames=["first", "last", "house"])
+        writer.writerow({"first": "first", "last": "last", "house": "house"})
+        for row in op:
+            writer.writerow({"first": row['first'], "last": row['last'], "house": row['house']})
 
-
-def check_command_line_arg():
-    # Check how many elements in the command line
+def command_line_check():
     if len(sys.argv) < 3:
         sys.exit("Too few command-line arguments")
     if len(sys.argv) > 3:
         sys.exit("Too many command-line arguments")
-    # Check if it is a CSV file
     if ".csv" not in sys.argv[1] or ".csv" not in sys.argv[2]:
         sys.exit("Not a CSV file")
 
