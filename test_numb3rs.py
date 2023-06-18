c3c8b8a95a4bde8b98faa9bc0da49e8de437ebf0
anxkhn <anxkhn@users.noreply.github.com>
2023-06-18 12:08:06 +0530

    automated commit by check50 [check50=True]

--- test_numb3rs.py
+++ test_numb3rs.py
@@ -1,7 +1,22 @@
 from numb3rs import validate
 
-def test_shorten():
-    assert validate("192.168.43.1") == True
-    assert validate("0.0.0.0") == True
-    assert validate("17.15.21.10") == True
-    assert validate("256.0.0.1") == False
+def main():
+    test_format()
+    test_range()
+
+def test_format():
+    assert validate(r'cat') == False
+    assert validate(r'127') == False
+    assert validate(r'127.0') == False
+    assert validate(r'127.0.1') == False
+    assert validate(r'127.0.1.2') == True
+
+def test_range():
+    assert validate(r'255.255.255.255') == True
+    assert validate(r'512.1.1.1') == False
+    assert validate(r'1.512.1.1') == False
+    assert validate(r'1.1.512.1') == False
+    assert validate(r'1.1.1.512') == False
+
+if __name__ == "__main__":
+    main()
